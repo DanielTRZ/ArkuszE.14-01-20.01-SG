428e4d5265814eb75eb8c4b88f7fd373fe1c8e01
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-04-01 02:26:31 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
 <html lang="pl">
-
   <head>
-
      <meta charset="utf-8">
      <title>Nasz sklep komputerowy</title>
-
      <meta name="description" content="Opis zawartości strony dla wyszukiwarek">
      <meta name="keywords" content="słowa, kluczowe, opisujące, zawartość">
      <meta name="author" content="Jan Programista">
      <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
      <link rel="stylesheet" href="styl8.css">
-   
-
   </head>
   <body>
 <div id="menu">
@@ -55,6 +50,5 @@
 <div id="stopka4">
     Stronę wykonał:00000000000 
 </div>
-  </body>
-
+</body>
 </html>
